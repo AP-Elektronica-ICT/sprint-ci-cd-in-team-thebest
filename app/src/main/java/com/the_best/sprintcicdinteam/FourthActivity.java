0c032b9fa6122bf462f632a27af9b6d68c58851d
Davy Cools <davy.cools@student.ap.be>
2018-12-20 15:46:13 +0100

    Toevoegen clickcounter
    button toegevoegd + textview en deze laten optellen

--- app/src/main/java/com/the_best/sprintcicdinteam/FourthActivity.java
+++ app/src/main/java/com/the_best/sprintcicdinteam/FourthActivity.java
@@ -2,12 +2,22 @@ package com.the_best.sprintcicdinteam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FourthActivity extends AppCompatActivity {
-
+    private TextView number;
+    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+        number = findViewById(R.id.editText);
+    }
+
+    public void IncreaseCounter(View view) {
+        counter = Integer.parseInt(number.getText().toString());
+        counter++;
+        number.setText(counter);
     }
 }
